0edb2a1586ec7f65140591f9f0b4627167a26cd4
LuisBalan <jlbalcz@gmail.com>
2021-10-07 15:20:39 -0500

    add solution of homework 03

--- tareas/tarea03/js/script2.js
+++ tareas/tarea03/js/script2.js
@@ -10,98 +10,34 @@
 
 //función generadora de numeros enteros entre 0 y 15 (para el sistema hexadecimal)
 
-function getRandomNumber(){
-    return parseInt(Math.random()*(33-127)+33)
+function getRandomNumber(min, max){
+    return parseInt(Math.random()*(max-min) + min)
 }
 
-console.log(getRandom(33, 127))
-
-// función para elegir un número del sistema hexadecimal de manera aleatoria
-
-// function getRandomHexDigit(){   
-//     let arrayHex = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
-//     let randomNumber = getRandom(0, 16)
-//     console.log(getRandom(33, 126))
-//     let randomHexDigit = (arrayHex[randomNumber])
-//     return randomHexDigit
-// }
-
-// función para formar la parte hexadecimal del unicode
-
-// function getHexPart(){
-//     let hexPart = ""
-//     for (index = 1 ; index <= 4 ; index ++){
-//         hexPart += getRandomHexDigit()
-//     }
-//     // console.log(hexPart)
-//     return hexPart
-// }
-
-// getHexPart()
-
-// función para ingresar dígitos e imprimir cadena de caracteres
-
-function userRandomCharString(){
-    let numberOfChar = parseInt(window.prompt("Ingrese un número entero mayor"))
-    let randomChars = ""
-    let index = 1
-    while(index <= numberOfChar){
-        randomChars = randomChars + "\\u"+`${getHexPart()}`
+function randomCharPrinter(){
+    let randomCharString = ""
+    
+    index = 0
+    while(index < numberOfChar){
+        randomCharString += String.fromCharCode(getRandomNumber(33, 127))
         index ++
     }
-    // let userRandomChars = randomChars
- 
- 
-    console.log(`${randomChars}`)
+    return console.log(`La cadena formada por ${numberOfChar} caracteres aleatorios es:\n ${randomCharString}`)
 }
 
-// userRandomCharString()
+function randomCharGen(){
+    numberOfChar = window.prompt("Ingrese el número de caracteres que desea imprimir: ")
+    if(isNaN(numberOfChar)){
+        console.log("No es posible elaborar una cadena. Por favor inserte un número.")
+    }else if(numberOfChar-parseInt(numberOfChar) != 0){
+        console.log(`No se puede elaborar una cadena con ${numberOfChar} elementos. Inserte un número entero igual o mayor que 1.`)
+    }else if(numberOfChar == 0){
+        console.log(`No se puede elaborar una cadena con ${numberOfChar} elementos. Inserte un número entero igual o mayor que 1.`)
+    }else{
+        randomCharPrinter()
+    }    
+}
 
+randomCharGen()
 
-   // console.log(index)
-
-
-// console.log("\uA108\u2A26")
-
-// for (index = 1; index <= 4 ; index++ ){
-//     randomChars = randomChars + `${getHexPart()}`
-// }
-
-// userRandomCharString()
-
-
-
-// console.log("\u0158\uE80E")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-// console.log(getHexPart())
-
-// let numberOfChar = parseInt(window.prompt("Ingrese un número entero: "))
-// let userCharString = ""
-// for (index = 1; index <= numberOfChar; index++){
-//     userCharString += getRandomHexChar()
-// }
-
-// console.log(userCharString)
-
-// console.log(getRandomHexChar())
-
-// let randomNumber = Math.random()
-// console.log(randomNumber)
-// let randomIntNumber = Math.floor((randomNumber*10))
-// console.log(randomIntNumber)
-// // 
+// randomCharPrinter()
